9bfd398cd08023df1eb28533e69c3ac8f9cb374c
wzk <1123989147@qq.com>
2019-02-12 14:53:17 +0800

    学习类属性和实例属性

--- python0212/day1.py
+++ python0212/day1.py
@@ -1,1 +1,12 @@
 print("你好呀")
+class Person():
+    name="wzk"
+    def __init__(self,_age):
+        self.age=_age
+
+s1=Person(1)
+s2=Person(2)
+Person.name="wk"
+print(s2.name,s2.age)
+print(s1.name,s1.age)
+
