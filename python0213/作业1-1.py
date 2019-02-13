b5e86da7e5ab82e64f4017b5b2a90f64c83f9ea2
wzk <1123989147@qq.com>
2019-02-13 20:39:18 +0800

    动态添加实例方法和静态and 类方法

---
+++ python0213/作业1-1.py
@@ -0,0 +1,23 @@
+# Iterable判断是否可以迭代
+from collections.abc import Iterable
+#Iterator 判断是否是迭代器
+from collections.abc import Iterator
+li=[1,2,3,4,5,6,7,8]
+tp=(1,2,3,4,5,6,8,1,)
+dic={"id":1,"name":"wzk","addr":"xinxiang"}
+STR="abcdefg"
+INT=100
+#判断是否可以迭代
+print(isinstance(li,Iterable))
+print(isinstance(tp,Iterable))
+print(isinstance(dic,Iterable))
+print(isinstance(STR,Iterable))
+print(isinstance(INT,Iterable))
+#判断是否是迭代器
+print(isinstance(li,Iterator))
+print(isinstance(tp,Iterator))
+print(isinstance(dic,Iterator))
+print(isinstance(STR,Iterator))
+print(isinstance(INT,Iterator))
+
+
